fa80fea2af9a5d10cb5daac5e553c1901bfe2dfc
Areas <tototok@163.com>
2018-01-25 16:15:49 +0800

    add icon; adjust window size;

--- main.js
+++ main.js
@@ -8,7 +8,18 @@ const url = require('url')
 let mainWindow
 
 function createWindow () {
-    mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false, resizable: false })
+  const display = electron.screen.getPrimaryDisplay()
+
+  let width = Math.floor(display.workAreaSize.width * 0.7)
+  let height = Math.floor(display.workAreaSize.height * 0.9)
+
+  if (width > 1440) width = 1440
+  if (height > 900) height = 900
+
+  const iconfiletype = process.platform == 'win32' ? 'ico' : 'png'
+  const icon = path.resolve(__dirname, `icon.{iconfiletype}`)
+
+  mainWindow = new BrowserWindow({ width: 800, height: 600, icon: icon, frame: false, resizable: false })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
